565222657c1aae234e07c1f85ab6fd5887a0a925
JuhoKon <kontiainen97@hotmail.fi>
2019-12-25 16:18:28 +0200

    AA

--- app.js
+++ app.js
@@ -58,9 +58,12 @@ app.use(function(err, req, res, next) {
   });
 });
 if (process.env.NODE_ENV === "production") {
+  // Set static folder
   app.use(express.static("client/build"));
+
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
+
 module.exports = app;
